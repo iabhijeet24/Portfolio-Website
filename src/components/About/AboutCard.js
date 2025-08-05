123eaaedb980d7ab8982466b8bf3fe45f23ddb2c
Abhijeet Singh <inabhijeet241107@gmail.com>
2025-08-05 18:00:38 +0530

    Updated About card

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,31 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Abhijeet Singh </span>
+            from <span className="purple"> Kolkata, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am currently pursuing Diploma in Computer Science and Engineering from Brainware University, Kolkata.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+
+            Beyond coding, I'm passionate about a variety of creative and enriching activities that keep me curious and inspired:    
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              ✨Exploring new technologies and ideas
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              ✍️Blogging about tech and experiences
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              🌍Traveling to discover vast cultures and places
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Abhijeet</footer>
         </blockquote>
       </Card.Body>
     </Card>
